47f624d645e921fbc6cffcaba4f220f5a30ee7c6
Harsh Kapadia <harshgkapadia@gmail.com>
2022-10-21 18:20:11 -0400

    Add styling and headings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Counter from "./Counter";
+import "./App.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <Counter/>
-    </div>
-  );
+	return (
+		<div className="app">
+			<h1>App Component</h1>
+			<Counter />
+		</div>
+	);
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,34 +1,40 @@
 import React, { useEffect, useState } from "react";
-import './Counter.css';
+import "./Counter.css";
 
 function Counter() {
-  // Set the initial count state to zero, 0
-  const [count, setCount] = useState(0);
+	// Set the initial count state to zero, 0
+	const [count, setCount] = useState(0);
 
-  // useEffect(() => {
-  //   console.log('Count is: ', count);
-  // }, [count]);
+	// useEffect(() => {
+	//   console.log('Count is: ', count);
+	// }, [count]);
 
-  // Create handleIncrement event handler
-  const handleIncrement = () => {
-    setCount(prevCount => prevCount + 1);
-  };
+	// Create handleIncrement event handler
+	const handleIncrement = () => {
+		setCount((prevCount) => prevCount + 1);
+	};
 
-  //Create handleDecrement event handler
-  const handleDecrement = () => {
-    setCount(prevCount => prevCount - 1);
-  };
-  return (
-    <div className="Counter">
-      <h1>Hello</h1>
-      <div>
-        <button class="button_change" onClick={handleDecrement}>-</button>
-        <h2>{count}</h2>
-        <button class="button_change" onClick={handleIncrement}>+</button>
-      </div>
-      <button class="button_set" onClick={() => setCount(0)}>Reset</button>
-    </div>
-  );
+	//Create handleDecrement event handler
+	const handleDecrement = () => {
+		setCount((prevCount) => prevCount - 1);
+	};
+	return (
+		<div className="Counter">
+			<h1>Counter Component</h1>
+			<div>
+				<button class="button_change" onClick={handleDecrement}>
+					-
+				</button>
+				<h2>{count}</h2>
+				<button class="button_change" onClick={handleIncrement}>
+					+
+				</button>
+			</div>
+			<button class="button_set" onClick={() => setCount(0)}>
+				Reset
+			</button>
+		</div>
+	);
 }
 
 export default Counter;
