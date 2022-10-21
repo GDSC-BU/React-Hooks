8312986dc001847f5d8e63da0fbdb9de080fca64
ScarlettLi <scartt42@gmail.com>
2022-10-21 13:29:07 -0400

    create counter

---
+++ src/Counter.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import './Counter.css';
+
+function Counter() {
+  // Set the initial count state to zero, 0
+  const [count, setCount] = useState(0);
+
+  // useEffect(() => {
+  //   console.log('Count is: ', count);
+  // }, [count]);
+
+  // Create handleIncrement event handler
+  const handleIncrement = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  //Create handleDecrement event handler
+  const handleDecrement = () => {
+    setCount(prevCount => prevCount - 1);
+  };
+  return (
+    <div className="Counter">
+      <h1>Hello</h1>
+      <div>
+        <button class="button_change" onClick={handleDecrement}>-</button>
+        <h2>{count}</h2>
+        <button class="button_change" onClick={handleIncrement}>+</button>
+      </div>
+      <button class="button_set" onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter;
